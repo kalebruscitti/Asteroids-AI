53361c60144c7f2ebd51882f41b5980ec2d0acac
Kaleb Ruscitti <kaleb@ruscitti.ca>
2017-04-27 23:59:56 -0400

    Cleaned up code + added better comments

--- src/AIBrain.py
+++ src/AIBrain.py
@@ -2,16 +2,17 @@ import numpy as np
 from asteroids import *
 from ai import *
 from soundManager import *
-#number of games to play
+#number of games to play when training
 trials = 2
 
 class Brain():
 
 	def __init__(self):
+		#store scores
 		self.score_array = np.empty([0,1])
 
 	def runAI(self):
-
+		#run one instance of the game
 		initSoundManager()
 		game = Asteroids()
 		game.playGame()
@@ -20,6 +21,7 @@ class Brain():
 
 AI = AI()
 Brain = Brain()
+#repeat training
 for i in range(trials):
 	Brain.runAI()
 
--- src/ai.py
+++ src/ai.py
@@ -1,29 +1,29 @@
 import numpy as np
 from asteroids import *
 # Machine Learning AI for Asteroids
-# This AI is designed to work with a modified version of Pythonic Asteroids (see "original readme.txt")
-# by Kaleb Ruscitti (kaleb.ruscitti.ca)
+# This AI is designed to work with a modified version of Pythonic Asteroids (see "original readme.txt") - Original Author: Nick Redshaw
+# by Kaleb Ruscitti (kaleb.ruscitti.ca) and Joshua Varga
 
 class AI():
     def __init__(self):
+        #bool array where each index corresponds to a possible action.
         self.input_array = np.array([False,False,False,False,False])
-        #bool array for each action
-        self.p_array = np.random.random_sample((5,))
-        #probability array for each action
+
+        #change this number to the number of neurons in the first layer
         self.neurons=5
-        #change this number to the number of neurons
         self.generateNeurons()
 
     def sendInput(self, baddie_array, ship_angle, ship_position):
-        print("pos:")
-        print(ship_position)
-        self.input_array = np.array([False,False,False,False,False])
-        n = 0
+        #reset the inputs from the last cycle
+        self.input_array = np.array([False,False, False,False,False])
+        n = 0 #counter
         left = 0
         for neuron in self.Neuron_array:
-            print("Neuron " + str(n))
             out = neuron.output(baddie_array, ship_angle, ship_position)
+            #out is the value of \sigma(w dot I - b)
             if out > 0.5:
+                #this if tree is required to choose between turning left or right
+                #whichever of the two actions is higher is performed.
                 if n == 1:
                     left = out
                 if n == 2:
@@ -38,30 +38,31 @@ class AI():
 
     def generateNeurons(self):
         self.Neuron_array = np.empty([0,1])
+        #neuron array allows us to iterate over them easily
         for n in range(self.neurons):
             neuron = Neuron()
             self.Neuron_array = np.append(self.Neuron_array, neuron)
 
-class Neuron():
+class Neuron(): #sigmoid neuron
     def __init__(self):
+        ##To do: recieve calculated inputs using gradient descent
         self.angle_weights = np.random.rand()
         self.pos_weights = np.random.rand(1,2)
         self.baddie_weights = np.random.rand(2,)
         self.bias = 0.75
 
     def sigmoid(self, x):
-        return 1/(1 + np.exp(x))
+        return 1/(1 + np.exp(x)) #sigma(x) where x = (w dot I - b)
 
 
     def output(self, baddie_array, ship_angle, ship_position):
+        #function calculates x = (w dot I - b) and sends it to the sigmoid function
+        #w = weights, I = inputs, b = bias
         self.x = 0
         self.x += np.dot(self.angle_weights,ship_angle)
         self.x += np.dot(self.pos_weights, ship_position)
         for rock in baddie_array:
             self.x += np.dot(rock, self.baddie_weights)
-        print("x2 " + str(self.x))
         self.x -= self.bias
         self.x *= -1
-        print("final X " + str(self.x))
-        print(self.sigmoid(self.x))
         return self.sigmoid(self.x)
